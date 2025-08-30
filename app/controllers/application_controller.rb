44e34dc616a2a9a6c03ce39598dd07424a70baf7
Muhammad Adhi Satria <adhistria@Muhammads-MacBook-Pro.local>
2025-08-30 11:55:22 +0700

    add authenticate user method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,20 @@
 class ApplicationController < ActionController::API
+  before_action :authenticate_user!
+
+  private
+
+  def authenticate_user!
+    set_user
+    return if @user
+
+    render json: { error: "Not Authorized" }, status: :unauthorized
+  end
+
+  def set_user
+    user_id = request.headers['X-User-ID']
+    return if user_id.blank?
+
+    @user = User.find_by(id: user_id)
+  end
+
 end
